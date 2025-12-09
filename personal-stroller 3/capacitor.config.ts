7c77940dc8ca65bf77f18b55e19bb5e0d64d3317
leh253 <gontara.habib@outlook.com>
2025-12-09 22:30:48 +0100

    Add files via upload

---
+++ personal-stroller 3/capacitor.config.ts
@@ -0,0 +1,26 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.personal_products_co.personal_stroller_v6',
+  appName: 'Personal Stroller',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    Keyboard: {
+      resize: 'body',
+      style: 'dark',
+      resizeOnFullScreen: true,
+    },
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#151b2b",
+      showSpinner: false,
+      androidSplashResourceName: "splash",
+      iosSplashResourceName: "Default"
+    }
+  }
+};
+
+export default config;
